586268a7719824eba54598c94b190053b5d2973a
dev <dev@DevVM-ip179998-v12>
2017-10-26 10:44:04 +0200

    com a class ValidarLigon

---
+++ src/main/java/com/redoutevant/gitproj/ValidarLogin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redoutevant.gitproj;
+
+/**
+ *
+ * @author dev
+ */
+public class ValidarLogin {
+    
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
